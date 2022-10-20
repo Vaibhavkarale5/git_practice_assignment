af9126cd6466d3416f4f97c3ecd32e24a170d6fb
abhishekyadav0888 <shraddhayadav0888@gmail.com>
2022-10-20 11:44:00 +0530

    Prime number code is write.

--- prime.js
+++ prime.js
@@ -1,6 +1,14 @@
+let num= 19;
 
+let fact = 0;
 
-
-
-
-
+for(let i=1 ; i<=num ; i++) {
+    if(num % i == 0) {
+        fact++;
+    }
+}
+    if(fact == 2) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
